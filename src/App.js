e7771f65c29de3612369bfaf6c9a545d0a1dd960
RamyaR1408 <110910802+RamyaR1408@users.noreply.github.com>
2023-01-04 21:02:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import LandingPage from "./screens/LandingPage/LandingPage";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen/RegisterScreen";
+import { useState } from "react";
+function App() {
+  const [search, setSearch] = useState("");
+
+  return (
+    <Router>
+      <Header setSearch={(s) => setSearch(s)} />
+      <main className="App">
+        <Route path="/" component={LandingPage} exact />
+        <Route path="/login" component={LoginScreen} />
+        <Route path="/register" component={RegisterScreen} />
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
